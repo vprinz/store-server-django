89df213baf7d7118860ca065ab8c2a26b4d35b97
Vokler <valerypavlikov@yandex.ru>
2022-08-23 16:11:06 +0300

    created IndexView

--- products/views.py
+++ products/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic.base import TemplateView
 
 from products.models import ProductCategory, Product, Basket
 from django.core.paginator import Paginator
 
 
-def index(request):
-    context = {'title': 'Store'}
-    return render(request, 'products/index.html', context)
+class IndexView(TemplateView):
+    template_name = 'products/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data()
+        context['title'] = 'Store'
+        return context
 
 
 def products(request, category_id=None, page_number=1):
--- store/urls.py
+++ store/urls.py
@@ -18,11 +18,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from products.views import index
+from products.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('', IndexView.as_view(), name='index'),
     path('products/', include('products.urls', namespace='products')),
     path('users/', include('users.urls', namespace='users')),
 ]
