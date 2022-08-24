013c4d80aac6316df37c54725b35331d50cc3978
Vokler <valerypavlikov@yandex.ru>
2022-08-24 15:09:20 +0300

    added EMAIL_BACKEND

--- store/settings.py
+++ store/settings.py
@@ -131,3 +131,7 @@ AUTH_USER_MODEL = 'users.User'
 LOGIN_URL = '/users/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Sending emails
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
