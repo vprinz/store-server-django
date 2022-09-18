1ea2c18547855b40020eff86fd93f6975ab620fc
Vokler <valerypavlikov@yandex.ru>
2022-09-18 14:22:47 +0300

    added PostgreSQL

--- store/settings.py
+++ store/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store_db',
+        'USER': 'store_username',
+        'PASSWORD': 'store_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
