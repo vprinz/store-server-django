00c8517cfe465a9fe82966c72d27d04707076509
Vokler <valerypavlikov@yandex.ru>
2022-10-15 12:07:12 +0300

    static for prod

--- store/settings.py
+++ store/settings.py
@@ -183,9 +183,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-)
+if DEBUG:
+    STATICFILES_DIRS = (
+        BASE_DIR / 'static',
+    )
+else:
+    STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
