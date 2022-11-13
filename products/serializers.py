8a2a259f7f9cf76f3d85e9b0f1910a1cb1f68f68
Vokler <valerypavlikov@yandex.ru>
2022-11-13 22:57:47 +0200

    Отображение категорий и пагинация

--- products/serializers.py
+++ products/serializers.py
@@ -4,6 +4,8 @@ from products.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.SlugRelatedField(slug_field='name', read_only=True)
+
     class Meta:
         model = Product
         fields = ('id', 'name', 'description', 'price', 'quantity', 'image', 'category')
