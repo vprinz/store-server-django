ba47022a3c1d15d31744a37d620ea3526d342910
Vokler <valerypavlikov@yandex.ru>
2022-11-14 10:07:26 +0200

    changed ImageField for Product model - required=False

--- products/models.py
+++ products/models.py
@@ -24,7 +24,7 @@ class Product(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.PositiveIntegerField(default=0)
-    image = models.ImageField(upload_to='products_images')
+    image = models.ImageField(upload_to='products_images', null=True, blank=True)
     stripe_product_price_id = models.CharField(max_length=128, null=True, blank=True)
     category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
 
